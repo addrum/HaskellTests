0c863183968ebceb58464f2e93f667fb91513797
Adam Short <adamshort1994@gmail.com>
2015-10-08 10:42:08 +0100

    Add new functions

--- FirstScript.hs
+++ FirstScript.hs
@@ -4,17 +4,14 @@ size :: Integer
 size = 12+13
 
 -- The function to square an integer
-
 square :: Integer -> Integer
 square n = n*n
 
 -- The function to double an integer
-
 double :: Integer -> Integer
 double n = 2*n
 
 -- The function to double the square of an integer
-
 doubleSquare :: Integer -> Integer
 doubleSquare n = double(square n)
 
@@ -23,9 +20,25 @@ fourthPower :: Integer -> Integer
 fourthPower n = square (square n)
 
 -- The function to return the factorial of an integer
-factorial :: Integer -> Integer
-factorial n = product [1..n]
+factorialInteger :: Integer -> Integer
+factorialInteger n = product [1..n]
+
+-- The function to return the factorial of an integer
+factorialInt :: Int -> Int
+factorialInt n = product [1..n]
 
 -- The function to return the hypotenuse of two integers
 norm :: Double -> Double -> Double
 norm x y = sqrt ((x*x) + (y*y))
+
+-- function returns true if all args are different
+threeDifferent :: Integer -> Integer -> Integer -> Bool
+threeDifferent x y z = x /= y && x /= z && y /= z
+
+-- returns golden ratio
+phi :: Float
+phi = (1 + sqrt 5) / 2
+
+-- function returns the fractional (decimal) part of a float
+fractionOfFloat :: Float -> Float
+fractionOfFloat x = x - (fromIntegral(floor x))
